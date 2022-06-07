ceb6e30745e108708dd680cf442cfd483073a175
Luã Streit <lua.streit@protonmail.com>
2022-06-06 22:42:36 -0400

    added new configs

--- experiment_configs/featurization.py
+++ experiment_configs/featurization.py
@@ -3,6 +3,6 @@ from experiment_configs.default import get_config as default_config
 
 def get_config():
     cfg = default_config()
-    cfg.task = 'classification'
+    cfg.task = 'featurization'
     cfg.project = f'validyna-{cfg.task}-cluster'
     return cfg
--- experiment_configs/featurization_forecasting.py
+++ experiment_configs/featurization_forecasting.py
@@ -3,6 +3,7 @@ from experiment_configs.default import get_config as default_config
 
 def get_config():
     cfg = default_config()
+    cfg.pre_task = 'featurization'
     cfg.task = 'forecasting'
-    cfg.project = f'validyna-{cfg.task}-cluster'
+    cfg.project = f'validyna-{cfg.pre_task}-to-{cfg.task}-cluster'
     return cfg
