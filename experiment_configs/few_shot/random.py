01e7f097839fae13dc2af4cc6a13da79ba20e869
Luã Streit <lua.streit@protonmail.com>
2022-07-08 14:35:11 -0400

    added baseline few shot experiment

---
+++ experiment_configs/few_shot/random.py
@@ -0,0 +1,14 @@
+from experiment_configs.default import get_config as default_config
+from experiment_configs.few_shot.common import get_config as common_config
+
+
+def get_config():
+    cfg = common_config()
+    placeholders = default_config('placeholders')
+
+    cfg.project = 'few-shot-random'
+
+    placeholders['f_all'] = lambda a: a in ['SprottE', 'Arneodo', 'Lorenz', 'Sakarya', 'NuclearQuadrupole']
+    placeholders['f_excluded'] = lambda a: a == 'SprottE'
+
+    return cfg
