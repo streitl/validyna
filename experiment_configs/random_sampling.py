1ff873250c77a3902653835f8b25b2bf0f2a6b98
Luã Streit <lua.streit@protonmail.com>
2022-08-13 16:19:56 -0400

    sending to cluster

--- experiment_configs/random_sampling.py
+++ experiment_configs/random_sampling.py
@@ -6,6 +6,9 @@ from experiment_configs.default import get_config as default_config
 
 def get_config():
     cfg = default_config()
+    placeholders = default_config('placeholders')
+
+    cfg.run_suffix = placeholders['task']
 
     cfg.sampling_ratio = 0.75
 
