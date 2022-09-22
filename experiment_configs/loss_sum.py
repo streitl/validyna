e35d957ee8980557d0eabe673799439e9b95b9f4
Luã Streit <lua.streit@protonmail.com>
2022-09-22 20:31:44 +0200

    improved loss sum experiment

--- experiment_configs/loss_sum.py
+++ experiment_configs/loss_sum.py
@@ -6,7 +6,15 @@ from experiment_configs.default import get_config as default_config
 def get_config():
     cfg = default_config()
 
-    cfg.runs = list(map(lambda d: ConfigDict(d), [{'task': 'all'}]))
-    cfg.loss_coefficients = {'mse': 0.8, 'cross': 0.1, 'triplet': 0.1}
+    cfg.runs = list(map(lambda d: ConfigDict(d), [
+        {
+            'task': 'all',
+            'run_suffix': f'{p:.2f}',
+            'loss_coefficients': {'mse': p, 'cross': (1-p) / 2, 'triplet': (1-p) / 2}
+        }
+        for p in [1 / 3, 0.5, 2 / 3, 0.8]
+    ]))
+
+    cfg.models = filter(lambda t: t[0] == 'GRU', cfg.models)
 
     return cfg
